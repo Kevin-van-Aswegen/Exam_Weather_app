a2a6bf2314e9016fd1f34b02c97e20c5b93a38f4
Kevin van Aswegen <st10454553@imconnect.edu.za>
2024-06-10 09:32:07 +0200

    Added back button to go between screens, as well as unique backround for Main and detail screens. Added buttons for each day of the week.

--- app/src/main/java/com/example/examweatherapp/Detailed_Screen.kt
+++ app/src/main/java/com/example/examweatherapp/Detailed_Screen.kt
@@ -1,31 +1,20 @@
 package com.example.examweatherapp
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Main_Screen : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+class Detailed_Screen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main_screen)
+        setContentView(R.layout.activity_detailed_screen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val backButton = findViewById<Button>(R.id.backButton)
-
-        backButton.setOnClickListener {
-            val intent = Intent(this, Login_Screen::class.java)
-            startActivity(intent)
         }
     }
 }
