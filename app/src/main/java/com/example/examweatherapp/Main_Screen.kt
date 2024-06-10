a86e6862549c84df1b786f5e7634e9594345f5a8
Kevin van Aswegen <st10454553@imconnect.edu.za>
2024-06-10 10:23:46 +0200

    Added textviews for each day of the week to dislay minimum and maximum temps, numbers were generated with random() code.

--- app/src/main/java/com/example/examweatherapp/Main_Screen.kt
+++ app/src/main/java/com/example/examweatherapp/Main_Screen.kt
@@ -4,10 +4,12 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Random
 
 class Main_Screen : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -22,10 +24,56 @@ class Main_Screen : AppCompatActivity() {
         }
 
         val backButton = findViewById<Button>(R.id.backButton)
+        val random = Random()
+
+        val monMin = findViewById<TextView>(R.id.mondayMin)
+        val monMax = findViewById<TextView>(R.id.mondayMax)
+
+        val tueMin = findViewById<TextView>(R.id.tuesdayMin)
+        val tueMax = findViewById<TextView>(R.id.tuesdayMax)
+
+        val wedMin = findViewById<TextView>(R.id.wednesdayMin)
+        val wedMax = findViewById<TextView>(R.id.wednesdayMax)
+
+        val thuMin = findViewById<TextView>(R.id.thursdayMin)
+        val thuMax = findViewById<TextView>(R.id.thursdayMax)
+
+        val friMin = findViewById<TextView>(R.id.fridayMin)
+        val friMax = findViewById<TextView>(R.id.fridayMax)
+
+        val satMin = findViewById<TextView>(R.id.saturdayMin)
+        val satMax = findViewById<TextView>(R.id.saturdayMax)
+
+        val sunMin = findViewById<TextView>(R.id.sundayMin)
+        val sunMax = findViewById<TextView>(R.id.sundayMax)
 
         backButton.setOnClickListener {
             val intent = Intent(this, Login_Screen::class.java)
             startActivity(intent)
         }
+
+        val randomMin = (0..12).random()
+        val randomMax = (17..35).random()
+
+        monMin.text = randomMin.toString()
+        monMax.text = randomMax.toString()
+
+        tueMin.text = randomMin.toString()
+        tueMax.text = randomMax.toString()
+
+        wedMin.text = randomMin.toString()
+        wedMax.text = randomMax.toString()
+
+        thuMin.text = randomMin.toString()
+        thuMax.text = randomMax.toString()
+
+        friMin.text = randomMin.toString()
+        friMax.text = randomMax.toString()
+
+        satMin.text = randomMin.toString()
+        satMax.text = randomMax.toString()
+
+        sunMin.text = randomMin.toString()
+        sunMax.text = randomMax.toString()
     }
 }
